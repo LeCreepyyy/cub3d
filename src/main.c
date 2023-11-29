42db2391e5be95560dba42ed5a0c3f394522032d
ubuntuVM <benjaming@outlook.fr>
2023-11-29 17:56:08 +0100

    textures loading

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vpoirot <vpoirot@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bgaertne <bgaertne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 11:00:10 by vpoirot           #+#    #+#             */
-/*   Updated: 2023/11/29 15:03:36 by vpoirot          ###   ########.fr       */
+/*   Updated: 2023/11/29 16:25:56 by bgaertne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,14 @@ void	init_data(t_data *data)
 	data->player_pos[0] = 0;
 	data->player_pos[1] = 0;
 	data->player_pos[2] = 0;
+	data->imgs.wall_north = NULL;
+	data->imgs.wall_south = NULL;
+	data->imgs.wall_east = NULL;
+	data->imgs.wall_west = NULL;
+	data->imgs.floor = NULL;
+	data->imgs.ceiling = NULL;
+	data->imgs.mp_player = NULL;
+	data->imgs.mp_wall = NULL;
 }
 
 void	ft_exit(char *err, int code)
