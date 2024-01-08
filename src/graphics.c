0dc1d9394325543afcf59a6177773b00a41f5251
Benjamin Gaertner <bgaertne@k1r2p2.42mulhouse.fr>
2024-01-08 10:54:14 +0100

    seems good?

--- src/graphics.c
+++ src/graphics.c
@@ -6,7 +6,7 @@
 /*   By: bgaertne <bgaertne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 22:17:47 by bgaertne          #+#    #+#             */
-/*   Updated: 2024/01/07 16:28:48 by bgaertne         ###   ########.fr       */
+/*   Updated: 2024/01/08 10:53:30 by bgaertne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 void	graphics(t_data *data)
 {
-	int	i;
-	float	distance;
+	int				i;
+	long long int	distance;
 
 	i = -1;
 	while (++i < WIDTH)
 	{
-		if (pow(i, 2) > 0)
+		if (i == 0)
 			distance = sqrt(pow(data->rays[i], 2) - pow(i, 2));
 		else
 			distance = data->rays[i];
 		if (i == 0)
-			printf("0, %f\n", distance);
+			printf("0, %lld\n", distance);
 		if (i == 839)
-			printf("839, %f\n", distance);
+			printf("839, %lld\n", distance);
 		if (i == 1679)
-			printf("1679, %f\n", distance);
+			printf("1679, %lld\n", distance);
 	}
 }
